120adba51fd2a41a8c607a53dc953ee07b13600a
苏苏 <d131415000@163.com>
2023-08-10 23:56:21 +0800

    :construction_worker:

--- src/main/java/com/susu/controller/DynamicController.java
+++ src/main/java/com/susu/controller/DynamicController.java
@@ -9,10 +9,7 @@ import com.susu.damian.Result;
 import com.susu.dao.DynamicDao;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,5 +37,18 @@ public class DynamicController {
         Integer code = dynamics != null ? Code.GET_OK : Code.GET_ERR;
         String msg = dynamics != null ? "查询成功" : "数据查询失败，请重试！";
         return new Result(dynamics, code, msg);
+    }
+
+    /**
+     * 动态的发布
+     * @return
+     */
+    @PostMapping
+    public Result publish(@RequestBody Dynamic dynamic){
+        System.out.println(dynamic);
+        int flag = dynamicDao.insert(dynamic);
+        Integer code = flag != 0 ? Code.SAVE_OK : Code.SAVE_ERR;
+        String msg = flag != 0 ? "提交成功！" : "数据提交失败，请重试！";
+        return new Result(null,code,msg);
     }
 }
--- src/main/java/com/susu/damian/Dynamic.java
+++ src/main/java/com/susu/damian/Dynamic.java
@@ -1,6 +1,8 @@
 package com.susu.damian;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
@@ -20,6 +22,7 @@ import java.time.LocalDateTime;
 @ToString
 @TableName("tb_dynamic")
 public class Dynamic {
+    @TableId(type = IdType.AUTO)
     private Integer id;
     @TableField(value="create_time")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
