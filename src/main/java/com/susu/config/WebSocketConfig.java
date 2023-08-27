005bff810b8a2d25d20d094fe1c136bf097e6b33
苏苏 <d131415000@163.com>
2023-08-27 21:12:17 +0800

    :construction_worker:Introduce websocket

---
+++ src/main/java/com/susu/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package com.susu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @Date:2023/8/27 21:02
+ * @Created by Muqing
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
---
+++ src/main/java/com/susu/damian/Message.java
@@ -0,0 +1,20 @@
+package com.susu.damian;
+
+import lombok.*;
+
+/**
+ * @Date:2023/8/27 21:04
+ * @Created by Muqing
+ */
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Message {
+    private Long id;
+    private String name;
+    private String time;
+    private String msg;
+}
